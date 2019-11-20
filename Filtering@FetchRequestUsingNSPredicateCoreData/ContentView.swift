30a4508bd69a0e4886580b4111bc182873958fe3
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2019-11-20 16:07:42 +0200

    init

--- Filtering@FetchRequestUsingNSPredicateCoreData/ContentView.swift
+++ Filtering@FetchRequestUsingNSPredicateCoreData/ContentView.swift
@@ -7,10 +7,38 @@
 //
 
 import SwiftUI
+import CoreData
 
 struct ContentView: View {
+    @Environment(\.managedObjectContext) var moc
+    @FetchRequest(entity: Ship.entity(), sortDescriptors: [], predicate: NSPredicate(format: "Not name BEGINSWITH[c] %@", "e")) var ships: FetchedResults<Ship>
+    
     var body: some View {
-        Text("Hello World")
+        VStack {
+            List(ships, id:\.self) { ship in
+                Text(ship.name ?? "Unknown name")
+            }
+            
+            Button("Add Examples") {
+                let ship1 = Ship(context: self.moc)
+                ship1.name = "Enterprise"
+                ship1.universe = "Star Trek"
+                
+                let ship2 = Ship(context: self.moc)
+                ship2.name = "Defiant"
+                ship2.universe = "Star Trek"
+                
+                let ship3 = Ship(context: self.moc)
+                ship3.name = "Millenium Falcon"
+                ship3.universe = "Star Wars"
+                
+                let ship4 = Ship(context: self.moc)
+                ship4.name = "Executor"
+                ship4.universe = "Star Wars"
+                
+                try? self.moc.save()
+            }
+        }
     }
 }
 
